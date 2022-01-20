04f51f8949b1b35395195eef7501b00574deb6a3
Dakota Dunn <dakotadunn5@gmail.com>
2022-01-19 18:08:26 -0800

    added editing functions for due date and tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,78 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on('click', 'p', function(){
+  var text = $(this)
+  .text()
+  .trim();
+  var textInput = $("<textarea>")
+  .addclass("form-control")
+  .val(text);
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+})
 
+$(".list-group").on('blur',"textarea", function(){
+  var text = $(this)
+  .val()
+  .trim();
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  tasks[status][index].text = text;
+  saveTasks();
+
+  var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+  $(this).replaceWith(taskP);
+})
+
+$(".list-group").on('click', "span", function(){
+  var date = $(this)
+  .text()
+  .trim();
+
+  var dateInput = $("<input>")
+  .attr("type","text")
+  .addClass("form-control")
+  .val(date);
+
+  $(this).replaceWith(dateInput);
+
+  dateInput.trigger("focus");
+})
+
+$(".list-group").on('click', "input[type='text']", function(){
+  var date = $(this)
+  .val()
+  .trim();
+
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  tasks[status][index].date = date;
+  saveTasks();
+
+  var taskSpan = $("<span>")
+  .addClass("badge badge-primary badge-pill")
+  .text(date);
+
+  $(this).replaceWith(taskSpan);
+})
 
 
 // modal was triggered
@@ -77,7 +148,6 @@ $("#task-form-modal .btn-primary").click(function() {
       text: taskText,
       date: taskDate
     });
-
     saveTasks();
   }
 });
